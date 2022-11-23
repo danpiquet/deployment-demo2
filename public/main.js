030739363c952ae8b7843fc1b41811b377e7e09b
Dan Piquet <dan.piquet@gmail.com>
2022-11-23 15:04:41 -0700

    added new base url for AWS

--- public/main.js
+++ public/main.js
@@ -1,5 +1,14 @@
 const btn = document.querySelector('button')
+const btn2 = document.querySelector('#button2')
 
 const clickHandler = () => alert('The dog name is: Rosco')
 
 btn.addEventListener('click', clickHandler)
+
+const getDog = () => {
+    axios.get('http://ec2-35-92-127-22.us-west-2.compute.amazonaws.com/api/dog')
+    .then(res => alert(res.data))
+    .catch(err => console.log(err))
+}
+
+btn2.addEventListener('click',getDog)
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000,
    () => console.log(`server running on 4000`)
 )
+
+app.get('/api/dog', (req, res) => {
+   res.status(200).send('Lily')
+})
